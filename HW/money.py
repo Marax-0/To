a461d91350defdce3a1c57a9b0ee93d3a800f139
Marax-0 <work.ratchann@gmail.com>
2025-08-04 14:34:10 +0700

    up

--- HW/money.py
+++ HW/money.py
@@ -2,11 +2,11 @@ PRICE = 275
 n = int(input("Enter : "))
 change = n-PRICE
 if(n >= 100):
-    print(change//100)
+    print("แบงค์ 100 :",change//100,"ใบ")
     change %= 100
 if(n >= 10):
-    print(change//10)
+    print("เหรียญ 10 :",change//10,"เหรียญ")
     change %= 10
 if(n >= 1):
-    print(change//1)
+    print("เหรียญ 1 :",change//1,"เหรียญ")
     change %= 1
